f8f0f835c94fb97b5518ff784257c76862c8a97a
pankaj-kb <pankajb2601@gmail.com>
2023-06-27 22:49:46 +0530

    routed home compoment

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+    </Routes>
+  </Router>
 )
+
+function Home() {
+  return (
+    <div>
+      <h1>Home Route</h1>
+    </div>
+  )
+}
