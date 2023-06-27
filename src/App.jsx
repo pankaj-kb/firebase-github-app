cefc6c91bd5aad71a1074fc4acaa8333b7a23663
pankaj-kb <pankajb2601@gmail.com>
2023-06-27 22:38:05 +0530

    added react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return <>
+    <h1>Hello Demo</h1>
+  </>;
 }
 
 export default App;
