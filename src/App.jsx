43cc01c831ada8361f17b6193a4bad136a287167
pankaj-kb <pankajb2601@gmail.com>
2023-06-29 17:35:59 +0530

    removing practice files

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Home from "./components/Home.jsx";
-import Learn from "./components/Learn.jsx";
-import Course from "./components/Course.jsx";
-import Bundle from "./components/Bundle.jsx";
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Learn" element={<Learn />} />
-          <Route path="/Learn/Course" element={<Course />} />
-          <Route path="/Learn/Bundles" element={<Bundle />} />
-        </Routes>
-      </Router>
+    <h1>Hello World</h1>
     </>
   );
 }
