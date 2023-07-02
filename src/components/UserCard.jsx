d821c37bdce0403bc92cd4532e7e51995acd40e0
pankaj-kb <pankajb2601@gmail.com>
2023-07-02 22:32:21 +0530

    fixed the git profile issue

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -3,14 +3,14 @@ import React from "react";
 
 import { Card, CardBody } from "reactstrap";
 
-const UserCard = (user) => {
-  const url = "https://api.github.com/users/";
+const UserCard = ({ user }) => {
+  // const url = "https://api.github.com/users/";
 
   return (
     <Card className="text-center mt-3 mb-4">
       <img src={user.avatar_url} className="img-thumbnail" />
       <CardBody>
-        <div className="text-primary">{user.name}</div>
+        <div className="text-primary"> {user.name}</div>
         <div className="text-primary">{user.location}</div>
         <div className="text-primary">{user.bio}</div>
         <div className="text-primary">
