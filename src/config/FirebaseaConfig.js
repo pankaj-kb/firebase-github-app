82befebfd8cba10ee563641ce28988113ab4ed65
pankaj-kb <pankajb2601@gmail.com>
2023-07-02 22:40:23 +0530

    fixed exposed api Keys

--- src/config/FirebaseaConfig.js
+++ src/config/FirebaseaConfig.js
@@ -1,9 +1,9 @@
 export default {
-    apiKey: "AIzaSyDH52gJsNWfDCCjU84lQdnjhfzw-qjdVDs",
-    authDomain: "gitinfoapp.firebaseapp.com",
-    projectId: "gitinfoapp",
-    storageBucket: "gitinfoapp.appspot.com",
-    messagingSenderId: "492241169029",
-    appId: "1:492241169029:web:b7d321910629d989e497ec",
-    measurementId: "G-WSG25MNW0S"
-  };
+  apiKey: import.meta.env.apiKey,
+  authDomain: "gitinfoapp.firebaseapp.com",
+  projectId: "gitinfoapp",
+  storageBucket: "gitinfoapp.appspot.com",
+  messagingSenderId: "492241169029",
+  appId: import.meta.env.appId,
+  measurementId: "G-WSG25MNW0S",
+};
